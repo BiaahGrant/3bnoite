c202563686be1c84e0cbbef049f8550fb0e6f4dc
3B Modular Info <3binfo@basilides.local>
2018-09-25 21:33:18 -0300

    Inclusão das Classes Car, CarService e EditView

---
+++ javaWebPrimeFaces/src/main/java/dominio/Car.java
@@ -0,0 +1,68 @@
+package dominio;
+
+public class Car {
+
+    private String id;
+    private String brand;
+    private int year;
+    private String color;
+
+    public Car() {
+    }
+    
+
+    public Car(String id, String randomBrand, int randomYear, String randomColor) {
+        this.id = id;
+        this.brand = randomBrand;
+        this.year = randomYear;
+        this.color = randomColor;
+    }
+
+    Car(String randomId, String randomBrand, int randomYear, String randomColor, int randomPrice, boolean randomSoldState) {
+        this.id = id;
+        this.brand = randomBrand;
+        this.year = randomYear;
+        this.color = randomColor;
+    }
+
+    
+
+    public String  getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + "id=" + id + ", brand=" + brand + ", year=" + year + ", color=" + color + '}';
+    }
+    
+
+}
---
+++ javaWebPrimeFaces/src/main/java/dominio/EditView.java
@@ -0,0 +1,70 @@
+package dominio;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
+
+ 
+@ManagedBean(name="dtEditView")
+@ViewScoped
+public class EditView implements Serializable {
+     
+    private List<Car> cars1;
+    private List<Car> cars2;
+         
+    @ManagedProperty("#{carService}")
+    private CarService service;
+     
+    @PostConstruct
+    public void init() {
+        cars1 = service.createCars(10);
+        cars2 = service.createCars(10);
+    }
+ 
+    public List<Car> getCars1() {
+        return cars1;
+    }
+ 
+    public List<Car> getCars2() {
+        return cars2;
+    }
+     
+    public List<String> getBrands() {
+        return service.getBrands();
+    }
+     
+    public List<String> getColors() {
+        return service.getColors();
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
+     
+    public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Car Edited", ((Car) event.getObject()).getId());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Car) event.getObject()).getId());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public void onCellEdit(CellEditEvent event) {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+         
+        if(newValue != null && !newValue.equals(oldValue)) {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+}
